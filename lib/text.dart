a61b2b79567dec2d91a8b41176fba331dafb7c4e
KalilDev <kalilgamer0@gmail.com>
2019-08-18 23:14:12 -0300

    text: Add the real content

--- lib/text.dart
+++ lib/text.dart
@@ -1,32 +1,41 @@
 const String manganese = 'Manganês';
+const String manganeseImg = 'assets/mn.jpg';
 
-const List<String> titles = <String>[
-  'Manganês puro',
-  '70% Manganês e 30% Silicio',
-  '80% Manganês e 20% Ferro',
-  '98,5% Aluminio e 1,5% Manganês'
-];
+/// Map
+const String portoDeSantana =
+    'Em Porto de Santana-AP há risco de contaminação pela antiga barragem de rejeitos da mineradora de Manganês ICOMI. Análises indicaram que os teores de arsênio (As) e de manganês são elevados nos poços próximos.';
+const String fortunaLtda =
+    'Há em Niquelândia-GO uma mina, não de Níquel, mas de Manganês. Extrai-se 5.000 toneladas por mês, de um total de 8.000.000 de toneladas de minério cujo teor é 40%. O Brasil possue o minério de manganês com a maior concentracão no mundo, aliás.';
+const String mariana =
+    'Mariana é o local com a maior quantidade de minerio de manganês no Brasil por uma grande margem. Em 2005 tinha 4.058.300.000 toneladas, dos 4.079.400.000 do Brasil, equivalente a 99.5%. Porém destas "somente" 202.900.000 são lavraveis. Foi estimado que esta reserva durará até somente 2022.';
+const String genericMap0 =
+    'Devido ao seu amplo mercado consumidor é aplicações variadas, o Manganês constitui um elemento de importante papel na economia brasileira no que tange à exportação de commodities.';
+const String genericMap1 =
+    'No Brasil, as reservas de manganês representam 10% da quantidade mundial, sendo o 4° maior detentor deste elemento em seu território.';
+const String genericMap2 =
+    'As reservas de manganês são localizadas principalmente nos estado de Minas Gerais, Bahia, Pará e Mato Grosso do Sul.';
+const String genericMap3 =
+    'A extração de manganês é impactada de acordo com a demanda do mercado consumidor, e o Brasil exporta quantias consideráveis, gerando cerca de US\$221,9 milhões do período entre janeiro e agosto de 2017. Nos últimos anos, a exportação do manganês impacta positivamente a balança comercial brasileira é atrai cada vez mais o investimento por capital estrangeiro.';
+const String genericMap4 =
+    'Com a realização de pesquisas e descoberta de novas reservas, mais empregos são gerados e há o investimento em infraestrutura nas regiões próximas as minas para a realização da extração, como a construção de dutos, ferrovias, complexos imobiliários, portos, etc.';
+const String genericMap5 =
+    'No Brasil, a empresa Vale detém cerca de 70% da produção do minério e mineradoras estrangeiras são responsáveis pelo resto da extração é investimento em pesquisa na área mineira.';
 
-/*const List<String> texts = <String>[
-  'Manganes é um metal que é quebradiço',
-  'Manganes+Silicio é uma liga',
-  'Manganes+Ferro é uma liga',
-  'Aluminio+Manganês é uma liga'
-];*/
+/// Periodic table
+const String mainText =
+    '       O manganês tem diversas aplicações, sendo usado principalmente na indústria siderúrgica para produção de ligas e, também, na produção de pilhas.' +
+        '\n       Este constitui 0,1% da crosta terrestre, sendo o 12° elemento mais abundante e o 5° metal mais encontrado. Há reservas desiguais deste em países como África do Sul, Brasil, Ucrânia, Austrália, Índia, China e México.';
 
-const String manganeseImg = 'assets/manganes.jpg';
-const List<String> imgPaths = <String>[
-  manganeseImg,
-  'assets/simn.jpg',
-  'assets/femg.png',
-  'assets/almn.jpeg',
-];
-
-const String lipsum =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo quis nulla eu euismod. Donec finibus, ante pellentesque pharetra imperdiet, lacus nisl mattis augue, ac convallis sem arcu non risus. Praesent id semper magna. Proin mollis bibendum lacus, vel vulputate tortor congue ut. Nulla eu purus consequat, accumsan lectus at, mollis justo. Pellentesque at sapien nec sem suscipit feugiat a in purus. In sed arcu varius, feugiat diam id, ornare quam. Fusce tincidunt porttitor est, vitae rhoncus odio hendrerit ut.\nDonec semper viverra elementum. In dapibus elementum ligula, vel interdum nunc accumsan a. Phasellus at imperdiet leo. Integer sed dictum est. Aliquam ac lorem a ipsum porta sagittis. Pellentesque maximus egestas quam, sed pretium augue eleifend id. Vestibulum in ligula turpis. Fusce rhoncus sed metus ac aliquet.\nNunc feugiat elit a neque tristique, a lacinia magna cursus. Nulla dapibus neque diam, quis finibus sem convallis quis. Nam dapibus tellus ut magna bibendum, vel feugiat purus tincidunt. Nam quam mauris, tempus at velit ullamcorper, ultricies ullamcorper ex. Vestibulum id turpis sed lectus faucibus vulputate. Praesent sed consequat massa. Donec sagittis urna sit amet sem lobortis consectetur. Curabitur a accumsan nisi, vulputate bibendum mi. Duis tristique erat ut augue porta, vel vehicula lacus sagittis.\nDuis congue nec purus a ullamcorper. Morbi tincidunt non orci non vehicula. Vestibulum et est ac ipsum luctus pulvinar. Phasellus sodales justo mollis pretium molestie. Proin imperdiet gravida sem, sit amet varius mauris consectetur a. Cras ac nulla velit. Aenean tincidunt felis eget purus venenatis dictum.\nIn fringilla nisl non dui vestibulum, sit amet posuere est dictum. Vestibulum ut dui non quam efficitur sodales. In a sagittis eros. Praesent maximus arcu metus, id consequat nisl tincidunt vitae. Aenean condimentum viverra augue eget scelerisque. Pellentesque elit nibh, eleifend vel lobortis at, suscipit ac massa. Praesent quis neque feugiat sapien porta efficitur. Vivamus posuere justo et consectetur vulputate. In pellentesque vehicula venenatis. Nullam eget lacus urna.';
-const List<String> texts = <String>[
-  'Mama0' + lipsum + '\n' + lipsum,
-  'Mama1' + lipsum,
-  'Mama2' + lipsum,
-  'Mama3' + lipsum
-];
+/// Alloys
+const String mn =
+    '      Foi descoberto em 1774 pelo sueco Johan Gottlieb Gahn, ao isolar o elemento de um de seus óxidos usando carbono. É um agente removedor de enxofre e oxigênio e tem diversos compostos úteis como o dióxido de manganês, usado em pilhas, e o permanganato de potássio. É usado também em fertilizantes, tecnologias para energia sustentável e eliminação de impurezas.';
+const String stmn =
+    '       Mangalloy, Aço manganês, ou Aço Hadfield são as possíveis denominações para essa liga, composta de 11% a 15% de manganês, 0,8% a 1,25% de carbono e o restante de Ferro, como qualquer aço. Essa liga especifica possui muitas características interessantes, resultando em extenso uso industrial e doméstico. Essas são: Alta resistência a impactos, e alta resistência à abrasão. Isso resulta em extrema resistência a desgastes em geral, resultando em excelente durabilidade, já que, mesmo sendo duro, não é quebradiço.' +
+        '\n       Ligas de aço com manganês possuem propriedades muito distintas, dependendo da quantidade de manganês: Quase todos os aços possuem até 0,8% de Mn, entre 1% e 1,8% em ligas de alta resistência, mas, entre 1,5% e 5%, o aumento de manganês resulta no material ficando cada vez mais quebradiço. A partir de 5% a dureza e a ductibilidade começam a aumentar. Em 10%, o aço não é magnético em temperatura ambiente (Numa liga comum, a temperatura para ter essa característica é aprox. 727°C). Finalmente em 12% tanto a ductibilidade quanto a dureza chegam ao ponto máximo. A liga final tem um limite elástico regular, mas uma resistência à tração excepcional.';
+const String simn =
+    '       O Silício-Manganês é um composto químico feito pela mistura de silício com manganês, é encontrado naturalmente na terra. Quando combinados, esses elementos podem ser usados para produzir uma série de ligas de aço especiais. Silício-Manganês aumenta as propriedades naturais do aço, dando-lhe maior resistência e funções, bem como uma beleza estética. Isso significa que as ligas de manganês de silício podem ser usadas em aplicações além daquelas associadas a produtos de aço padrão. O silício manganês também pode ser conhecido como ferro silício manganês ou abreviado como SiMn.' +
+        '\n       Silício-Manganês pode ser usado sozinho ou combinado com outras substâncias para criar várias ligas. A proporção de silício para manganês também pode ser ajustada para fornecer ao aço as propriedades desejadas. Ligas de aço manganês de silício padrão contêm 14 a 16% de silício e até 68% de manganês. Eles também incluem pequenas quantidades de carbono, necessário para que o aço sofra reações químicas com o SiMn. Em aços inoxidáveis ou especiais, o teor de silício pode chegar a 30%.';
+const String femn =
+    'Ferromanganês é uma liga metálica composta, em sua maioria, por manganês. O uso principal é como desoxidante na produção de aço, removendo o oxigênio que decresce a qualidade da liga. Há três tipos especificados de Ferromanganês, um com baixo nível de carbono, um com nível médio de carbono, e o Ferromanganês padrão.';
+const String almn =
+    'Ligas de alumínio e manganês são o segundo maior uso deste elemento. Alumínio com aprox. 1,5% de manganês tem uma resistência à corrosão. As principais ligas são a que contem 0,8% e a que contem 1,5% de manganês, e o uso mais comum destas são latas de refri.';
